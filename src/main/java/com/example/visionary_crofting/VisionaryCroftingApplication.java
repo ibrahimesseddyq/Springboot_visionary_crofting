3942d6b7c0b30ccf8ee28438c7abe95d8974b06d
armanovxx <iebeat2018@outlook.com>
2022-11-23 14:53:19 +0100

    create project

---
+++ src/main/java/com/example/visionary_crofting/VisionaryCroftingApplication.java
@@ -0,0 +1,13 @@
+package com.example.visionary_crofting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionaryCroftingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisionaryCroftingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/visionary_crofting/VisionaryCroftingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.visionary_crofting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisionaryCroftingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
